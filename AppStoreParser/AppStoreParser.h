45ccd2c50ddfe527e280b3344bf08aa487c8269b
Luo Sheng <luosheng1986@gmail.com>
2015-10-12 09:09:45 +0800

    Initial Commit

---
+++ AppStoreParser/AppStoreParser.h
@@ -0,0 +1,19 @@
+//
+//  AppStoreParser.h
+//  AppStoreParser
+//
+//  Created by Luo Sheng on 15/10/12.
+//  Copyright © 2015年 Pop Tap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AppStoreParser.
+FOUNDATION_EXPORT double AppStoreParserVersionNumber;
+
+//! Project version string for AppStoreParser.
+FOUNDATION_EXPORT const unsigned char AppStoreParserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppStoreParser/PublicHeader.h>
+
+
